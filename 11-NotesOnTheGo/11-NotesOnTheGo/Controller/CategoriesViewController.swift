241007ba54ebc1f756f4b95de563f813321242e9
Juan Gabriel <juangabriel@frogames.es>
2018-08-12 18:19:07 +0200

    Proyecto de las notas con contenido hardcoded y métodos de las tablas implementados

---
+++ 11-NotesOnTheGo/11-NotesOnTheGo/Controller/CategoriesViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  11-NotesOnTheGo
+//
+//  Created by Juan Gabriel Gomila Salas on 09/08/2018.
+//  Copyright © 2018 Frogames. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UICollectionViewController {
+
+    let categoriesArray = ["Compras", "Tareas de la casa", "Ocio"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK:- Métodos de Collection View Data Source
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoriesArray.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath) as! CategoryCollectionViewCell
+        
+        cell.categoryLabel.text = categoriesArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ShowNoteList", sender: self)
+    }
+    
+}
+
